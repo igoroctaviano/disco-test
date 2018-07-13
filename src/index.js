0448aa0ba5e017529c547e0daded4ce3be2e3d20
Igor Octaviano <igoroctaviano@gmail.com>
2018-07-13 09:20:19 -0300

    Setup React with Webpack and Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Testing');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const title = "Disco!";
+
+ReactDOM.render(
+  <div>{ title }</div>,
+  document.getElementById("app")
+);
