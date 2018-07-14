14b6e7fcbada7dd596d4d50d12f97736b7a0c5d7
Igor Octaviano <igoroctaviano@gmail.com>
2018-07-14 19:17:18 -0300

    Chain less-loader with the css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,12 @@ module.exports = {
         use: ["babel-loader"]
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"]
       }
     ]
   },
